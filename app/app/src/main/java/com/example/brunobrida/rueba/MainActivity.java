62aea50d9ee3f41a405607877e47d18c84722116
Bruno De Brida <bruno.debrida@gmail.com>
2017-06-27 21:28:36 -0300

    Autenticação
    Implementado autenticação

--- app/app/src/main/java/com/example/brunobrida/rueba/MainActivity.java
+++ app/app/src/main/java/com/example/brunobrida/rueba/MainActivity.java
@@ -1,34 +1,77 @@
 package com.example.brunobrida.rueba;
 
+import android.os.StrictMode;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.InputStream;
+import java.io.OutputStream;
+import android.widget.Toast;
+import org.json.JSONObject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
+    String data="";
+    String status = "";
+    int tmp;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
     }
 
     public void onButtonClick(View v){
-        if(v.getId() == R.id.login){
+
+        try {
+            EditText ID = (EditText)findViewById(R.id.ID);
+            String user = ID.getText().toString();
+            EditText passwd = (EditText)findViewById(R.id.password);
+            String password = passwd.getText().toString();
+
+            URL url = new URL("http://rueba.site/api/token");
+            String urlParams = "matricula="+user+"&senha="+password;
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            OutputStream os = urlConnection.getOutputStream();
+            os.write(urlParams.getBytes());
+            os.flush();
+            os.close();
+            InputStream is = urlConnection.getInputStream();
+            while ((tmp=is.read())!=-1) {
+                data += (char) tmp;
+            }
+            is.close();
+            urlConnection.disconnect();
+        } catch(Exception e) {
+            Toast.makeText(getApplicationContext(), "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        try {
+            JSONObject jObject = new JSONObject(data);
+            status = jObject.getString("status");
+        } catch(Exception e) {
+            Toast.makeText(getApplicationContext(), "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        if(status.equals("OK")){
             Intent i = new Intent(MainActivity.this, home.class);
             startActivity(i);
+            Toast.makeText(getApplicationContext(), "Login OK!", Toast.LENGTH_SHORT).show();
         }
+        else
+            Toast.makeText(getApplicationContext(), "Matrícula ou Senha inválida(s).", Toast.LENGTH_SHORT).show();
+
+        data = "";
+        status = "";
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
